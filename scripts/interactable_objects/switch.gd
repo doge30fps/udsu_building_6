0e8a68cc04a2a1458ae81caa2f0a721eca350993
doge30fps <roma.suntzov@yandex.ru>
2026-04-20 21:06:04 +0400

    switch/lamps work fine. projector works too. a little adjustments to the structure of main scene

--- scripts/interactable_objects/switch.gd
+++ scripts/interactable_objects/switch.gd
@@ -2,14 +2,18 @@ extends StaticBody3D
 
 var toggle = false
 var interactable = true
-@export var animation_player: AnimationPlayer
+@onready var animation_player_switch: AnimationPlayer = $AnimationPlayer
+@export var animation_player_omnilight: AnimationPlayer
+@export var lamp_material: StandardMaterial3D
 
 func interact():
 	if interactable == true:
 		interactable = false
 		toggle = !toggle
 		if toggle == false:
-			animation_player.play("close")
+			animation_player_switch.play("close")
+			animation_player_omnilight.play("close")
 		if toggle == true:
-			animation_player.play("open")
+			animation_player_switch.play("open")
+			animation_player_omnilight.play("open")
 		interactable = true
