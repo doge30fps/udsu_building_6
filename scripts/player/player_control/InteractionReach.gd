ebc23bc5d226ac13da2f1f1f832e09358cdc59b5
doge30fps <roma.suntzov@yandex.ru>
2026-04-23 00:22:05 +0400

    added crosshair

--- scripts/player/player_control/InteractionReach.gd
+++ scripts/player/player_control/InteractionReach.gd
@@ -1,12 +1,15 @@
 extends RayCast3D
 
 @onready var interact_label : Label = $interact_label
+@onready var crosshair_label : Label = $crosshair
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$interact_label.visible = false
+	interact_label.visible = false
+	crosshair_label.visible = true
 	if is_colliding():
 		var hitobj = get_collider()
 		if hitobj.has_method("interact"):
-			$interact_label.visible = true
+			interact_label.visible = true
+			crosshair_label.visible = false
 			if Input.is_action_just_pressed("interact"):
 				hitobj.interact()
