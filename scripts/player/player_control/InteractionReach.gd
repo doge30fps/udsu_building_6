a56c6339162fae95ade2e3378c8846c40067bb7f
doge30fps <roma.suntzov@yandex.ru>
2026-04-23 00:11:23 +0400

    added interaction label

--- scripts/player/player_control/InteractionReach.gd
+++ scripts/player/player_control/InteractionReach.gd
@@ -1,7 +1,12 @@
 extends RayCast3D
+
+@onready var interact_label : Label = $interact_label
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	$interact_label.visible = false
 	if is_colliding():
 		var hitobj = get_collider()
-		if hitobj.has_method("interact") && Input.is_action_just_pressed("interact"):
-			hitobj.interact()
+		if hitobj.has_method("interact"):
+			$interact_label.visible = true
+			if Input.is_action_just_pressed("interact"):
+				hitobj.interact()
