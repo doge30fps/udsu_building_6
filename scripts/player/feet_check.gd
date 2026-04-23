e6101d8311c332eea038d6b4da9f8bf1facd9dd0
doge30fps <roma.suntzov@yandex.ru>
2026-04-23 18:27:43 +0400

    trying to make different sounds for different types of surfaces

---
+++ scripts/player/feet_check.gd
@@ -0,0 +1,27 @@
+extends RayCast3D
+
+# Этот сигнал будет оповещать игрока о том, что поверхность под ним изменилась
+signal surface_changed(surface_name: String)
+
+# Предыдущий тип поверхности, чтобы не спамить сигналами при каждом кадре
+var last_surface: String = ""
+
+func _physics_process(delta):
+	# Проверяем, есть ли коллизия у луча
+	if is_colliding():
+		# Получаем объект, в который упёрся луч
+		var collider = get_collider()
+		# Пытаемся прочитать метаданные "surface_type" у этого объекта
+		if collider.has_meta("surface_type"):
+			var current_surface = collider.get_meta("surface_type")
+			# Если тип поверхности изменился, отправляем сигнал
+			if current_surface != last_surface:
+				last_surface = current_surface
+				surface_changed.emit(current_surface)
+			print_debug("found meta_surface: ", current_surface)
+		else:
+			# Если у объекта нет метаданных, можно отправить сигнал о поверхности по умолчанию
+			if last_surface != "unknown":
+				last_surface = "unknown"
+				surface_changed.emit("unknown")
+			print_debug("unknown meta_surface!")
